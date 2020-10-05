952a57632bc1c22e3c255afcd46026afc42f6d61
Chelsea Nikkel <3.14@belomor.su>
2020-10-05 10:24:06 +0500

    Kirill mastodont

---
+++ listTest.py
@@ -0,0 +1,13 @@
+p = ["авокадо", "яблоки", "бананы", "морковь", "чипсы"]
+print(p)
+p.sort()
+print(p)
+p.append("рис")
+print(p)
+del p[3]
+print(p)
+p.reverse()
+p.insert(1, "сыр")
+print(p)
+print(p.index("бананы"))
+print(len(p))
