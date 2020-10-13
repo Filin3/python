cbec34efa16f56a5dc55f0cb108802ed12c9e794
Chelsea Nikkel <3.14@belomor.su>
2020-10-13 18:30:25 +0500

    new tasks (not working)

--- changeColorInSeconds.py
+++ changeColorInSeconds.py
@@ -3,10 +3,12 @@ sec = 5
 pickedColor = 4
 while sec < 147:
     if sec % 20 == 0:
+        pickedColor += 1
         if len(colorList)-1 <= pickedColor:
             pickedColor = 0
-        else:
-            pickedColor += 1
+        #else:
+        #    pickedColor += 1
     sec += 1
-    #print(sec, pickedColor, colorList[pickedColor])    
+    #print(sec, pickedColor, colorList[pickedColor])
+print(sec % 20)    
 print(sec, pickedColor, colorList[pickedColor])
--- storeStonksDef.py
+++ storeStonksDef.py
@@ -1,2 +1,6 @@
 def storeStonks(stonks, day):
-    return
+    if day <= 1:
+        return stonks
+    print(day, stonks)
+    return storeStonks(stonks * 2, day - 1)
+print(storeStonks(200, 30))
