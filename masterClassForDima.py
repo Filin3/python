c918522e75907d73b9fddb7910923db1fcd3e39c
Chelsea Nikkel <3.14@belomor.su>
2020-10-28 12:46:05 +0500

    fix misstakes and add round

--- masterClassForDima.py
+++ masterClassForDima.py
@@ -11,14 +11,14 @@ while True:
 d = b**2-4*a*c
 print("d: {}".format(d))
 if d > 0:
-    x1 = (-b+math.sqrt(d))/2*a
-    x2 = (-b-math.sqrt(d))/2*a
+    x1 = round((-b+math.sqrt(d))/2*a, 4)
+    x2 = round((-b-math.sqrt(d))/2*a, 4)
     if int(x1) == x1: x1 = int(x1)
     if int(x2) == x2: x2 = int(x2)
     print("x1: {}\nx2: {}".format(x1, x2))
 elif d == 0:
+    x = round((-b+math.sqrt(d))/2*a, 4)
     if int(x) == x: x = int(x)
-    x = (-b+math.sqrt(d))/2*a
     print("x: {}".format(x))
 else:
     print("Нет решения")
